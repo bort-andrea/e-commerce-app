9f276fefe443913634cfa6505ad00a6531ac65fe
bort-andrea <bortolotti.andrea93@gmail.com>
2024-11-14 15:35:27 +0100

    Configurato db e collegato all app

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const db = require('./db');
+
+app.use(express.json());
+
+app.get('/utenti', async (req, res) => {
+    try {
+      const result = await db.query('SELECT * FROM utenti');  
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Errore nella query:', error);
+      res.status(500).send('Errore nel server');
+    }
+  });
+
 app.listen(PORT, ()=>{
     console.log(`Server listen on port: ${PORT}`)
 })
