2b48803ef43e5af3fa13dd632576b418222162b8
bort-andrea <bortolotti.andrea93@gmail.com>
2024-11-15 11:01:30 +0100

    Sviluppate ulteriori endpoint: login, registrazione, carrello

--- app.js
+++ app.js
@@ -1,11 +1,86 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const PORT = 3000;
-
+const bcrypt = require('bcrypt');
+const passport = require('./passportConfig');
 const db = require('./db');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//configuro la sessione
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+//inizializzo Passport e le sessioni
+app.use(passport.initialize());
+app.use(passport.session());
+
+//endpoint per la registrazione
+//
+app.post('/registrati', async(req,res)=>{
+    try{
+        const {username, password, email, indirizzo} = req.body;
+
+        //controllo se l'username o la mail inseriti esistono già
+        const userExist = await db.query('SELECT * FROM utenti WHERE username = $1 OR email = $2',[username,email]);
+        if(userExist.rows.length > 0){
+            console.error("Username o email già utilizzati. Riprovare");
+            return res.status(400).json({error: "Username o email già utilizzati. Riprovare"});
+        }
+        //criptare password
+        const salt = await bcrypt.genSalt(10);
+        const hashPass = await bcrypt.hash(password,salt);
+        
+        //inserisco utente nel database e creo carrello corrispondente
+        const registerUser = await db.query("INSERT INTO utenti (username, password, email, indirizzo) VALUES($1, $2, $3, $4) RETURNING id",
+                                             [username, hashPass, email, indirizzo]);
+        if(registerUser.rowCount > 0){
+            const userId = registerUser.rows[0].id;
+            await db.query("INSERT INTO carrelli (id_utente) VALUES($1)",[userId]);
+            
+            // Rispondo con successo
+            res.json({ message: "Registrazione avvenuta con successo!" });
+        }else {
+            res.status(500).json({ error: "Errore nella registrazione dell'utente" });
+        };
+
+    }catch(errore){
+        console.error("Errore nella query:", errore);
+        res.status(500).send("Errore nel server");
+    }
+});
+
+//endpoint per il login
+//
+app.post('/login', async(req,res)=>{
+    passport.authenticate('local', (err,user,info)=>{
+        
+        if(err) return next(err);
+        
+        if(!user){
+            return res.status(401).json({message:info.message});
+        }
+
+        req.login(user, (err)=>{
+            if(err) return next(err);
+            return res.json({message: 'Login avvenuto con successo', user:{id: user.id, username: user.username}});
+        });
+    })(req,res,next);
+});
+
+//endpoint per il logout
+//
+app.post('/logout', async(req,res)=>{
+    req.logout((err)=>{
+        if(err) return res.status(500).json({message:"Errore durante il logout"});
+        res.json({message:"Logout avvenuto con successo"});
+    })
+});
 
 // endpoint per le richieste sulla tabella prodotti
 //
@@ -75,6 +150,30 @@ app.get('/carrello/:id', async (req, res) => {
         res.json(result.rows);
     } catch (error) {
         console.error('Errore nella query:', error);
+        res.status(500).send('Errore nel server');
+    }
+});
+
+app.post('/carrello/:user', async(req,res)=>{
+    try{
+        const {id_prodotto, quantita} = req.body;
+
+        // Verifico che i parametri obbligatori siano presenti
+        if (!id_prodotto || !quantita) {
+            return res.status(400).json({ error: "Parametri mancanti: id_prodotto e quantita sono obbligatori" });
+        }
+
+        //trovo l'id del carrello corrispondente all'utente
+        const carrello = await db.query(`SELECT id_carrello FROM carrelli WHERE id_utente = $1`,[req.params.user]);
+        if(carrello.rows.length === 0){
+            return res.status(400).json({error:"Utente non trovato"});
+        }
+        const id_carrello = carrello.rows[0].id_carrello;
+        await db.query(`INSERT INTO carrelli_dett (id_carrello, id_prodotto, quantita) 
+                                         VALUES ($1, $2, $3)`,[id_carrello, id_prodotto,quantita]);
+        res.status(201).json({message:"Prodotto inserito correttamente"});
+    }catch(error){
+        console.error("Errore nella query:",error);
         res.status(500).send('Errore nel server');
     }
 });
---
+++ passportConfig.js
@@ -0,0 +1,51 @@
+const db = require('./db');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+//definisco la strategia di autenticazione
+//
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}), async(username, password, done)=>{
+    try{
+        //cerco l'utente nel database
+        const user = await db.query("SELECT * FROM utenti WHERE username = $1",[username]);
+        if(user.rows.length === 0){
+            return done(null, false, {message: 'Utente non trovato - inserire l\'username corretto'});
+        }
+
+        const utente = user.rows[0];
+        //controllo password
+        const passMatch = await bcrypt.compare(password, utente.password);
+        if(!passMatch){
+            return done(null, false, {message: 'Inserire la password corretta'});
+        }
+        return done(null, utente);
+
+    }catch(error){
+        return done(error);
+    }
+});
+
+//serializzo e deserializzo l'utente
+//
+passport.serializeUser((utente,done)=>{
+    done(null, utente.id);
+});
+
+passport.deserializeUser(async(id,done)=>{
+    try{
+        const utente = await db.query("SELECT * FROM utenti WHERE id = $1",[id]);
+        if(utente.rows.length === 0){
+            return done(new Error('Utente non trovato'));
+        }
+        done(null,user.rows[0]);
+    }catch(errore){
+        done(errore);
+    }
+})
+
+//esporto la configurazione
+module.exports = passport;
